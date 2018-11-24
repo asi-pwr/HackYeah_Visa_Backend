433962a0c774064ce7e52211fc3b50e14fe456ef
Jakub Dorda <jak_dor@wp.pl>
2018-11-24 21:23:07 +0100

    working upload endpoint

---
+++ flaskr/__init__.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import Flask, request
+
+def create_app(test_config=None):
+    UPLOAD_FOLDER = '/upload'
+
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/uploader', methods = ['GET', 'POST'])
+    def upload_file():
+       if request.method == 'POST':
+          f = request.files['file']
+          f.save("img.png")
+          return 'file uploaded successfully'
+
+    return app
